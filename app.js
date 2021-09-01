388b6a9ec3f78cef77cb4874520a9046c55f3d6c
azadtamal <mrazadt@gmail.com>
2021-09-01 16:26:47 +0600

    github

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const myBtn = document.getElementById("myBtn");
+
 myBtn.addEventListener("click", (e) => {
   const userInput = document.getElementById("userInput").value;
   console.log(userInput);
+  const userName = userInput.split(" ").join("");
+  alert(userName);
+
+  fetch("https://api.github.com/users/" + userName)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 });
