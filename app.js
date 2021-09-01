8673f8c3f022c0629748d39a919a7c65a13a23d7
azadtamal <mrazadt@gmail.com>
2021-09-01 15:47:36 +0600

    github

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("azad");
+const myBtn = document.getElementById("myBtn");
+myBtn.addEventListener("click", (e) => {
+  const userInput = document.getElementById("userInput").value;
+  console.log(userInput);
+});
